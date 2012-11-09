8cae3a5f891dceb1e96d9cc6ec370a7c46a7b49e
Josh Palmer <joshpalmer123@gmail.com>
2012-11-09 04:35:41 +0000

    lol i lost my source code
    always copy to dropbox don't save as. heh

--- NumericalIntegration/Program.cs
+++ NumericalIntegration/Program.cs
@@ -8,15 +8,29 @@ namespace NumericalIntegration
     class Program
     {
         static void Main(string[] args)
-        {
-            System.IO.StreamWriter file = new System.IO.StreamWriter("D:\\Coding\\PHYS2320_Computing_2\\NumericalIntegration\\task2.csv");
-            file.WriteLine("t,Trapezoid 10, Simpson 10, Trapezoid 50,Simpson 50,Trapezoid 100,Simpson 100");
+        {           
+            // part A
+
             double start = 0;
             double end = 10;
+            int[] meshpoints = new int[]{ 10, 50, 100 };
+
+            Console.WriteLine("Integrating Xsquared between 0 and 10");
+            foreach (var item in meshpoints)
+            {
+                double[] values = ValuesToIntegrateOver(start, end, item, XSquared);
+                double trapezoid = TrapezoidRule(start, end, item, values);
+                double simpsons = SimpsonsRule(start, end, item, values);
+                Console.WriteLine("At T = " + end + "; For " + item + " meshpoints: \tTrapezeoid = " + trapezoid + " Simpsons = " + simpsons);
+            }
+
+            // part B
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter("D:\\Coding\\PHYS2320_Computing_2\\NumericalIntegration\\task2_fixed.csv");
+            file.WriteLine("t,Trapezoid 10, Simpson 10, Trapezoid 50,Simpson 50,Trapezoid 100,Simpson 100");
             double[] points = new double[]{ 1, 2, 3, 4, 5 };
             foreach (var endpoint in points)
             {
-                int[] meshpoints = new int[]{ 10, 50, 100 };
                 file.Write(endpoint + ",");
                 foreach (var item in meshpoints)
                 {
@@ -30,6 +44,13 @@ namespace NumericalIntegration
             }
             file.Close();
 
+            // part C
+
+            double[] xvalues = ValuesToIntegrateOver(start, 2, 100, XSquared);
+            double[] yvalues = ValuesToIntegrateOver(start, 2, 100, Y);
+            double result = SimpsonsRule(start, 2, 100, xvalues) * SimpsonsRule(start, 2, 100, yvalues) * 30 * 2;
+            Console.WriteLine("Integrating 30x^2y with 100 meshpoints using simpson's rule: " + result);
+
         }
 
         static double XSquared(double x)
@@ -39,10 +60,13 @@ namespace NumericalIntegration
 
         static double SinSquaredStuff(double t)
         {
-           double retval = 0.5 * Math.Pow(Math.Sin(t / Math.PI), 2);
-           return retval;
+           return 0.5 * Math.Pow(Math.Sin(t / Math.PI), 2);
         }
 
+        static double Y(double y)
+        {
+            return y;
+        }
         static double[] ValuesToIntegrateOver(double start, double end, int meshpoints, Func<double, double> Function)
         {
             /// <summary>
@@ -52,11 +76,12 @@ namespace NumericalIntegration
             
             double interval = (end - start) / meshpoints;
             double[] values = new double[meshpoints + 1];
-            int count = 0;
-            for (double x = start ; x <= end ; x+=interval)
+            double x = start;
+            for (int i = 0; i <= meshpoints; i++)
             {
-                values[count] = Function(x);
-                count++;
+                values[i] = Function(x);
+                x += interval;
+                
             }
             return values;
         }
